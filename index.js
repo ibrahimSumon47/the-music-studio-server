8c1e43283aa62a114234f6e562066a27cd95cb1e
ibrahimSumon47 <ibrahimsumon005@gmail.com>
2023-06-09 01:03:14 +0600

    user added in server

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.92d2eha.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -27,6 +48,12 @@ async function run() {
     await client.connect();
 
     const userCollection = client.db("musicStudio").collection("users");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '170h' })
+      res.send({ token });
+    });
 
     // Users Api
     app.get("/users", async (req, res) => {
